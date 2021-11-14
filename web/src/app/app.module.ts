56e4fe153f4747e730780c554204bf46ccc53236
romulo.braun <romulo.braun@crpinformatica.com.br>
2021-11-14 13:23:54 -0300

    Primeiro commit

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatSliderModule } from '@angular/material/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 @NgModule({
